9d953fad0784a2870e980db6af26e018745ee51c
Tiven123 <steven.alejandro@hotmail.com>
2015-06-17 00:11:53 -0600

    Camibios

--- src/Grafos/Nodo.java
+++ src/Grafos/Nodo.java
@@ -2,14 +2,75 @@ package Grafos;
 
 import java.util.ArrayList;
 
+/**
+ * Clase Nodo de un grafo
+ * @author Steven, Fabricio, Arturo, Wilson
+ * @category Estructura Distribuida
+ * @version 1.0
+ */
+//Clase nodo de un grafo contiene los metodos objetos nesesarios para el manejo de la misma
 public class Nodo {
 	
 	public Object dato;
 	public ArrayList<Nodo> vecinos;
 	public Boolean visitado = false;
 	
+	/**
+	 * Construstor de la clase
+	 * @param datoN
+	 */
 	public Nodo(Object datoN){
 		this.dato=datoN;
 		vecinos=new ArrayList<Nodo>();
 	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Object getDato() {
+		return dato;
+	}
+	
+	/**
+	 * 
+	 * @param dato
+	 */
+	public void setDato(Object dato) {
+		this.dato = dato;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<Nodo> getVecinos() {
+		return vecinos;
+	}
+	
+	/**
+	 * 
+	 * @param vecinos
+	 */
+	public void setVecinos(ArrayList<Nodo> vecinos) {
+		this.vecinos = vecinos;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Boolean getVisitado() {
+		return visitado;
+	}
+	
+	/**
+	 * 
+	 * @param visitado
+	 */
+	public void setVisitado(Boolean visitado) {
+		this.visitado = visitado;
+	}
+	
+	
 }
--- src/Grafos/Pila.java
+++ src/Grafos/Pila.java
@@ -1,5 +1,20 @@
 package Grafos;
 
-public class Pila {
+import java.util.ArrayList;
 
+public class Pila extends ArrayList {
+
+	public Pila(){
+		super();
+	}
+	
+	public void agregarNodo(Nodo nodo){
+		if(!this.contains(nodo)&&nodo.visitado!=true){
+			agregar(nodo);
+			mostrarContenido();
+		}
+	}
+	public void obtenerNodo(){
+
+	}
 }
