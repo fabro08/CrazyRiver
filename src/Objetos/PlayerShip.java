d49fc1ecc752de66fccd7931b0892b7c9e321912
Arturok <mchinchilla11@gmail.com>
2015-06-17 04:08:04 -0600

    Agregadas las clases BasicEnemyShip, PlayerShip, Bullets y Bridge, a falta de modificaciones para su correcto uso!!!

---
+++ src/Objetos/PlayerShip.java
@@ -0,0 +1,53 @@
+package Objetos;
+
+public class PlayerShip {
+	
+	public int life;
+	public int fuel;
+	public int bullets;
+	public int Btype; //Esta variable es saber si tiene un tipo especial de bala True y False bala Normal
+	
+	public PlayerShip (int vida,int combustible,int balas,int typo){
+		this.life= vida;
+		this.fuel=combustible;
+		this.bullets = balas;
+		this.Btype = typo;
+		
+	}
+	
+	/*********
+	 * 
+	 * Getters del objeto nave
+	 * 
+	 ********/
+	public int getLife(){
+		return life;	
+	}
+	public int getFuel(){
+		return fuel;	
+	}
+	public int getbullets(){
+		return bullets;	
+	}
+	public int getBtype(){
+		return Btype;	
+	}
+	/*********
+	 * 
+	 * Setters del objeto nave
+	 * 
+	 ********/
+	public void setLife(int vida){
+		this.life= vida;
+	}
+	public void setFuel(int combustible){
+		this.fuel=combustible;
+	}
+	public void  setBullet(int balas){
+		this.bullets=balas;
+	}
+	public void setBulleType(int tipoB){
+		this.Btype=tipoB;
+	}
+	
+}
