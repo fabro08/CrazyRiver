29e3a8c69c61459c136962fae134409eefc893a9
Tiven123 <steven.alejandro@hotmail.com>
2015-06-17 16:40:41 -0600

    Cambios en el grafo (creacion de un grafo dirigido y no dirigido)

--- src/Grafos/Grafo.java
+++ src/Grafos/Grafo.java
@@ -2,40 +2,92 @@ package Grafos;
 
 import java.util.ArrayList;
 
-
+/**
+ * 
+ * @author steven
+ *
+ */
 public class Grafo {
-	private final int NODOSMAXIMOS=20;
+    private int nodosMaximos;
 	public Nodo listaNodos[];
-	public int matrizAdyacencia[][];
+	public int matrizAdyacenciaDirijida[][];
+	public int matrizAdyacenciaNoDirijida[][];
 	public int cantidad;
 	
-	public Grafo(){
-		listaNodos= new nodo[NODOSMAXIMOS];
-		matrizAdyacencia=new int[NODOSMAXIMOS][NODOSMAXIMOS];
-        cantidad=0;
-        for (int i = 0; i < NODOSMAXIMOS; i++) {
-            for (int j = 0; j <NODOSMAXIMOS ; j++) {
-                matrizAdyacencia[i][j]=0;
+	/**
+	 * 
+	 */
+	public Grafo(int cantidadN){
+        nodosMaximos=cantidadN;
+		listaNodos= new Nodo[nodosMaximos];
+		matrizAdyacenciaDirijida=new int[nodosMaximos][nodosMaximos];
+        for (int i = 0; i < nodosMaximos; i++) {
+            for (int j = 0; j <nodosMaximos; j++) {
+                matrizAdyacenciaDirijida[i][j]=0;
+            }
+        }
+        matrizAdyacenciaNoDirijida=new int[nodosMaximos][nodosMaximos];
+        for (int i = 0; i < nodosMaximos; i++) {
+            for (int j = 0; j <nodosMaximos; j++) {
+                matrizAdyacenciaNoDirijida[i][j]=0;
             }
         }
 
 	}
-
-	public void crearEnlaces(Object nombreNodoPadre, Object nombreNodoHijo){
-		Nodo padre = buscarNodo(nombreNodoPadre);
-		Nodo hijo=buscarNodo(nombreNodoHijo);
-		if (padre!= null&&hijo!=null){
-			padre.setVecinos(hijo);
-		}
-	}
 	
-	public Nodo buscarNodo(Object nombreNodo){
-		Nodo auxiliar = null;
-		for(int i=0; i<listaNodo.size();i++){
-			if(((String)nombreNodo).equalsIgnoreCase((String)listaNodo.get(i).getDato())){
-				return listaNodo.get(i);
-			}
-		}
-		return auxiliar;
+	public void insertarNodo(Object datoN){
+        if (cantidad <= nodosMaximos) {
+            listaNodos[cantidad++]= new Nodo(datoN);
+        }
+        else{
+            System.out.println("Error: Grafo LLeno");
+        }
 	}
+
+    public void insertarAristaDirijida(int inicio, int fin){
+        if (inicio <= nodosMaximos && fin <= nodosMaximos) {
+            matrizAdyacenciaDirijida[inicio][fin]=1;
+        }
+        else {
+            System.out.println("Error: fuera del rango de la matriz ");
+        }
+    }
+    
+    public void insertarAristaNoDirijida(int inicio, int fin){
+        if (inicio <= nodosMaximos && fin <= nodosMaximos) {
+            matrizAdyacenciaNoDirijida[inicio][fin]=1;
+            matrizAdyacenciaNoDirijida[fin][inicio]=1;
+        }
+        else {
+            System.out.println("Error: fuera del rango de la matriz ");
+        }
+    }
+
+    public void imprimirNodos(){
+        for (int i = 0; i < listaNodos.length; i++) {
+        	if(listaNodos[i]!=null){
+        		System.out.println(listaNodos[i].getDato());
+        	}
+        }
+    }
+    
+    public void imprimirMatrizAdyacenciaDirijida(){
+        for (int i = 0; i < nodosMaximos; i++) {
+            System.out.print("[");
+            for (int j = 0; j <nodosMaximos ; j++) {
+                System.out.print(" " + matrizAdyacenciaDirijida[i][j]);
+            }
+            System.out.print("]\n");
+        }
+    }
+    
+    public void imprimirMatrizAdyacenciaNoDirijida(){
+        for (int i = 0; i < nodosMaximos; i++) {
+            System.out.print("[");
+            for (int j = 0; j <nodosMaximos ; j++) {
+                System.out.print(" " + matrizAdyacenciaNoDirijida[i][j]);
+            }
+            System.out.print("]\n");
+        }
+    }
 }
